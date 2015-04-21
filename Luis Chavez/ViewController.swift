0b5e78e1ee460bd295657c6eebc529fdd228fdab
Luis Armando Chávez Soto <lquetzalmx@gmail.com>
2015-04-20 23:17:21 -0500

    Initial Commit

---
+++ Luis Chavez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Luis Chavez
+//
+//  Created by Luis Armando Chávez Soto on 20/04/15.
+//  Copyright (c) 2015 me.luischavez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
