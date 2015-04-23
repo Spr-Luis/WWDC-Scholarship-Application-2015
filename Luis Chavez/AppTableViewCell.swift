6372b9a523b28c6a2933fa233aaffd04b5bac5b2
Luis Armando Chávez Soto <lquetzalmx@gmail.com>
2015-04-23 12:52:17 -0500

    Mockups done

---
+++ Luis Chavez/AppTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AppTableViewCell.swift
+//  Luis Chavez
+//
+//  Created by Luis Armando Chávez Soto on 23/04/15.
+//  Copyright (c) 2015 me.luischavez. All rights reserved.
+//
+
+import UIKit
+
+class AppTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Luis Chavez/TitleTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TitleTableViewCell.swift
+//  Luis Chavez
+//
+//  Created by Luis Armando Chávez Soto on 23/04/15.
+//  Copyright (c) 2015 me.luischavez. All rights reserved.
+//
+
+import UIKit
+
+class TitleTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
